176d7f689ce6baf2af193bbe9419cdb3dd511f72
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2025-03-06 19:51:31 +0530

    barebones Push Notifications electron setup using Pushy

---
+++ src/main.ts
@@ -0,0 +1,87 @@
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import started from "electron-squirrel-startup";
+import Pushy from "pushy-electron";
+import {config} from "dotenv";
+config();
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (started) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
+  }
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.webContents.on("did-finish-load", () => {
+    Pushy.alert(mainWindow, "Pushy device token: " + process.env.PUSHY_APP_ID); 
+    Pushy.register({ appId: process.env.PUSHY_APP_ID })
+      .then(async (deviceToken) => {
+        // Display an alert with device token
+        await fetch("http://localhost:4000/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: "divyam.7379@gmail.com",
+            deviceToken,
+          }),
+        });
+        Pushy.alert(mainWindow, "Pushy device token: " + deviceToken);
+      })
+      .catch((err) => {
+        // Display error dialog
+        Pushy.alert(mainWindow, "Pushy registration error: " + err.message);
+      });
+    Pushy.setNotificationListener((data) => {
+      // Display an alert with the "message" payload value
+      console.log(data);
+      Pushy.alert(mainWindow, "Received notification: " + JSON.stringify(data));
+    });
+    Pushy.listen();
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return(
+        <div>
+            <h1>Notify</h1>
+            <p>Get notified</p>
+        </div>
+    )
+}
